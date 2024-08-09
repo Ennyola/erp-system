794e8b926af494eef17eb93775015f16d31dd632
Enny <medunoyeeni@gmail.com>
2024-08-09 13:48:17 +0100

    added superuser script to build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+
+# create custom superuser based on the superuser  environment variables
+python manage.py create_superuser
